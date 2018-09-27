bafebca36a61169a8f567cd346626bcdff54a280
Scott Clayton <scottclayton84@gmail.com>
2018-09-26 23:59:33 -0500

    Compiled pages

--- src/Othello/Othello.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Othello from './Othello';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Othello />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Othello from './Othello/Othello';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Othello />, document.getElementById('root'));
-registerServiceWorker();
